418bf87bfdc7ab1b38aacc524982e3d36a34e4c2
aturan10 <alituran1021@gmail.com>
2021-11-11 22:26:30 -0600

    new proje started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import ChessBoard from './components/ChessBoard';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <ChessBoard />
+        
+    </div>
+
+
+  );
+}
+
+export default App;
